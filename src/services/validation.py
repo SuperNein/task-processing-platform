1e1211b4fc58969729f62376f83f655c72a24b75
Konstantin Smage <kpsmage@mai.education>
2026-04-26 17:45:30 +0300

    add: validation logging

--- src/services/validation.py
+++ src/services/validation.py
@@ -1,4 +1,9 @@
+import logging
+
 from typing import Type, Any, Protocol
+
+
+logger = logging.getLogger(__name__)
 
 
 class ProtocolValidator:
@@ -11,6 +16,7 @@ class ProtocolValidator:
 
     def validate(self, obj: Any) -> None:
         if not self.is_valid(obj):
+            logger.error(f"Validation failed: {obj} not {self._protocol.__name__}")
             raise TypeError(
                 f"'{type(obj).__name__}' is missing "
                 f"following '{self._protocol.__name__}' protocol member."
